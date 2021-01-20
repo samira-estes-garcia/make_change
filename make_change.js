d7414c7f37bb160233d063ceb1d80fee6953a9d7
samira-estes-garcia <samira.estes.garcia@gmail.com>
2021-01-19 19:29:01 -0600

    added coinList array to store each combo, printed combo using coinCount function, console.table to present data

--- make_change.js
+++ make_change.js
@@ -1,33 +1,52 @@
-function makeChange() {
-
-    let formVal = $("#total").val;
+$("#form").submit(function(event){
+    console.clear();
+    event.preventDefault();
+    
+    //capture form amount submission and convert to cents
+    let formVal = $("#total").val();
     console.log(formVal); 
     let totalCents = (formVal*100)
+    console.log(totalCents)
 
     // number of coins to make a dollar
     let numQ = totalCents/25;
     let numD = totalCents/10;
     let numN = totalCents/5;
-    let numP = totalCents/100;
+    let numP = totalCents;
+
+    //num coins to start with
+    let q = 0;
+    let d = 0;
+    let n = 0;
+    let p = 0;
+
+    //array to push all combos into
+    let coinList = [];
 
     //number of combinations
     count = 0;
 
-    for(let q = 0; q <= numQ; q++){
+    //function to intialize types of coins
+    function coinCount(quarter, dime, nickel, penny) {
+        this.quarter = quarter;
+        this.dime = dime;
+        this.nickel = nickel;
+        this.penny = penny;
+    } 
 
-        for(let d = 0; d <= numD; d++){
-    
+    for(let q = 0; q <= numQ; q++){
+         for(let d = 0; d <= numD; d++){
             for(let n = 0; n <= numN; n++){
-    
-                for(let p = 0; p <= numP; p++){
-    
-                    if( ((q * 25) + (d * 10) + (n * 5) + p) == totalCents ){
-    
-                        console.log("Q: "+q+" D: "+d+" N: "+n+" P: "+p );
+              for(let p = 0; p <= numP; p++){
+                    if(((q*25) + (d*10) + (n*5) + p ) == totalCents) {
+                        let combo = new coinCount(q, d, n, p);
+                        coinList.push(combo);
                         count++;
-                    }
+                      }
                 }
             }
         }
     }
-}
+    console.log(`Count: ${count}`);
+    console.table(coinList)
+});
