3a0da53d377c682d3fde7ec50760559776fb0d30
samira-estes-garcia <samira.estes.garcia@gmail.com>
2021-01-20 15:26:35 -0600

    added license and readme.md

---
+++ README.md
@@ -0,0 +1,14 @@
+# Make Change Algorithm App
+
+## An HTML, CSS, and JavaScript Application
+
+Make Change uses JavaScript and jQuery to calculate all the combinations that US coins and user entered currencies can make change for a user entered amount. Users have access to two forms. The first form allows users to enter a positive dollar amount and shows all the combinations to reach that amount using US coins. The other form allows users to enter their own made up or real currencies, assign them an amount, enter a positive currency amount and shows all the combinations to reach that amount using their own currencies.
+
+## Installation
+
+- Clone down this repo and type "open index.html" in your terminal
+- Press command + option + j to open developer console to view the combinations in a neat and efficient table format
+
+## License
+
+The app is available as open source under the terms of the [MIT License](https://opensource.org/licenses/MIT).
--- make_change.js
+++ make_change.js
@@ -78,6 +78,7 @@ $("#form-anarchy").submit(function(event) {
     console.log(currency); 
     let currencyArr = currency.split(",");
     console.log(currencyArr);
+    console.log(currencyArr.length);
 
     //display error if currency input is invalid
     //given more time I would make sure the inputs were string, num, string, num, etc
@@ -88,7 +89,7 @@ $("#form-anarchy").submit(function(event) {
     //capture form submission for amount to get change for, convert to "cents"
     let total = $("#currency-amount").val();
     totalCents = (total*100);
-    console.log(total);
+    //console.log(total);
 
     //display error if formVal is invalid aka negative number or NaN
     if (total < 0 || isNaN(total)) {
@@ -115,27 +116,27 @@ $("#form-anarchy").submit(function(event) {
     //how much is each type of currency worth? divide 1 unit by the value, multiply by 100, divide totalCents by worth
     let currOneWorth = (1/currOneVal)*100;
     currOneWorth = currOneWorth.toFixed(2);
-    console.log(`Currency One is worth: ${currOneWorth} of a unit`);
+    //console.log(`Currency One is worth: ${currOneWorth} of a unit`);
     let numCurrOne = totalCents/currOneWorth; 
 
     let currTwoWorth = (1/currTwoVal)*100;
     currTwoWorth = currTwoWorth.toFixed(2);
-    console.log(`Currency Two is worth: ${currTwoWorth} of a unit`);
+    //console.log(`Currency Two is worth: ${currTwoWorth} of a unit`);
     let numCurrTwo = totalCents/currTwoWorth; 
 
     let currThreeWorth = (1/currThreeVal)*100;
     currThreeWorth = currThreeWorth.toFixed(2);
-    console.log(`Currency Three is worth: ${currThreeWorth} of a unit`);
+    //console.log(`Currency Three is worth: ${currThreeWorth} of a unit`);
     let numCurrThree = totalCents/currThreeWorth; 
 
     let currFourWorth = (1/currFourVal)*100;
     currFourWorth = currFourWorth.toFixed(2);
-    console.log(`Currency Four is worth: ${currFourWorth} of a unit`);
+    //console.log(`Currency Four is worth: ${currFourWorth} of a unit`);
     let numCurrFour = totalCents/currFourWorth; 
 
     let currFiveWorth = (1/currFiveVal)*100;
     currFiveWorth = currFiveWorth.toFixed(2);
-    console.log(`Currency Five is worth: ${currFiveWorth} of a unit`);
+    //console.log(`Currency Five is worth: ${currFiveWorth} of a unit`);
     let numCurrFive = totalCents/currFiveWorth; 
 
     //let num currencies to start with
@@ -152,36 +153,64 @@ $("#form-anarchy").submit(function(event) {
     count = 0;
 
     //function to intialize types of coins
-    function coinCount(currOneName, currTwoName, currThreeName, currFourName, currFiveName) {
-        this.currOneName = currOneName;
+    function coinCount(currencyArr, currTwoName, currThreeName, currFourName, currFiveName) {
+        this.currOneName = currencyArr;
         this.currTwoName = currTwoName;
         this.currThreeName = currThreeName;
         this.currFourName = currFourName;
         this.currFiveName = currFiveName;
     }
 
-    //loop through each type of coin to see if coins needed to make a dollar is reached
-    //increment and move to next coin
-//     for(let a = 0; a <= numCurrOne; a++){
-//         for(let b = 0; b <= numCurrTwo; b++){
-//            for(let c = 0; c <= numCurrThree; c++){
-//              for(let d = 0; d <= numCurrFour; d++){
-//                  for(let e = 0; e <= numCurrFive; e++) {
-//                     if(((a*25) + (d*10) + (n*5) + p ) == totalCents) {
-//                         //set individual combo to a coin count obj, push into coinList, increment count
-//                         let combo = new coinCount(q, d, n, p);
-//                         coinList.push(combo);
-//                         count++;
-//                       }
-//                  }
-//                }
-//            }
-//        }
-//    }
+    //loop through each type of currency to see if currency needed to make a unit is reached
+    //increment and move to next currency
+
+    if (currencyArr.length == 10) {
+        for(let a = 0; a <= numCurrOne; a++){
+            for(let b = 0; b <= numCurrTwo; b++){
+                for(let c = 0; c <= numCurrThree; c++){
+                    for(let d = 0; d <= numCurrFour; d++){
+                        for(let e = 0; e <= numCurrFive; e++) {
+                            if(((a*currOneWorth) + (b*currTwoWorth) + (c*currThreeWorth) + (d*currFourWorth) + (e*currFiveWorth)) == totalCents) {
+                                //set individual combo to a coin count obj, push into coinList, increment count
+                                let combo = new coinCount(a, b, c, d, e);
+                                coinList.push(combo);
+                                count++;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    } else if (currencyArr.length == 8) {
+        for(let a = 0; a <= numCurrOne; a++){
+            for(let b = 0; b <= numCurrTwo; b++){
+                for(let c = 0; c <= numCurrThree; c++){
+                    for(let d = 0; d <= numCurrFour; d++){
+                        if(((a*currOneWorth) + (b*currTwoWorth) + (c*currThreeWorth) + (d*currFourWorth)) == totalCents) {
+                            //set individual combo to a coin count obj, push into coinList, increment count
+                            let combo = new coinCount(a, b, c, d);
+                            coinList.push(combo);
+                            count++;
+                        }
+                    }
+                }
+            }
+        }
+    } else {
+        for(let a = 0; a <= numCurrOne; a++){
+            for(let b = 0; b <= numCurrTwo; b++){
+                for(let c = 0; c <= numCurrThree; c++){
+                    if(((a*currOneWorth) + (b*currTwoWorth) + (c*currThreeWorth)) == totalCents) {
+                        //set individual combo to a coin count obj, push into coinList, increment count
+                        let combo = new coinCount(a, b, c);
+                        coinList.push(combo);
+                        count++;
+                    }
+                }
+            }
+        }
+    }
    //console.log count and console.table coinList to format it in a readable way
    console.log(`Number of Combinations / Count: ${count}`);
    console.table(coinList)
-
-
-
 })
